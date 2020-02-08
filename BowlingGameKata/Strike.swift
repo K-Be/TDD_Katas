1cac16ca072e01b84f48e0e72a3e319f286364dd
Andrew Romanov <scalli-k-be@ya.ru>
2020-02-08 18:30:37 +0700

    added Strike class and non passed tests for isActive for strike

---
+++ BowlingGameKata/Strike.swift
@@ -0,0 +1,19 @@
+//
+//  Strike.swift
+//  BowlingGameKata
+//
+//  Created by Andrew Romanov on 08.02.2020.
+//  Copyright © 2020 Andrew Romanov. All rights reserved.
+//
+
+import Foundation
+
+
+struct Strike {
+	let startRollIndex : Int
+	
+	func isActive(forRoll roll : Int) -> Bool {
+		return false
+	}
+}
+
