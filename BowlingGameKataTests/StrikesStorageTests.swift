cdf13d504ed133eaf60a98eb64549b7169cb1031
Andrew Romanov <scalli-k-be@ya.ru>
2020-02-08 20:04:45 +0700

    added test for multiple strikes

--- BowlingGameKataTests/StrikesStorageTests.swift
+++ BowlingGameKataTests/StrikesStorageTests.swift
@@ -29,6 +29,22 @@ class StrikesStorageTests: XCTestCase {
 		XCTAssert(storage.countActiveStrikes(onRoll: 6) == 0)
 	}
 	
+	
+	func testMultiplyStrikes() {
+		let storage = StrikesStorage()
+		storage.addStrike(withRoll: 1)
+		storage.addStrike(withRoll: 2)
+		storage.addStrike(withRoll: 5)
+		
+		XCTAssert(storage.countActiveStrikes(onRoll: 1) == 0)
+		XCTAssert(storage.countActiveStrikes(onRoll: 2) == 1)
+		XCTAssert(storage.countActiveStrikes(onRoll: 3) == 2)
+		XCTAssert(storage.countActiveStrikes(onRoll: 4) == 1)
+		XCTAssert(storage.countActiveStrikes(onRoll: 5) == 0)
+		XCTAssert(storage.countActiveStrikes(onRoll: 6) == 1)
+		XCTAssert(storage.countActiveStrikes(onRoll: 8) == 0)
+	}
+	
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
