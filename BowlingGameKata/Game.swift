f720da4ba4957e977826eca1b6ba43ba4da1ea93
Andrew Romanov <scalli-k-be@ya.ru>
2020-02-08 17:11:58 +0700

    added interface class

---
+++ BowlingGameKata/Game.swift
@@ -0,0 +1,22 @@
+//
+//  Game.swift
+//  BowlingGameKata
+//
+//  Created by Andrew Romanov on 08.02.2020.
+//  Copyright © 2020 Andrew Romanov. All rights reserved.
+//
+
+import Foundation
+
+public class Game {
+	
+	
+	public func roll(_ pinsCount:Int) {
+		
+	}
+	
+	public func score() -> Int {
+		return 0;
+	}
+}
+
