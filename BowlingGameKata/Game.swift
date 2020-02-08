db2762c15021597386481c1c0cd97e62b9b96d78
Andrew Romanov <scalli-k-be@ya.ru>
2020-02-08 17:57:47 +0700

    make strike scores test passed

--- BowlingGameKata/Game.swift
+++ BowlingGameKata/Game.swift
@@ -10,13 +10,25 @@ import Foundation
 
 public class Game {
 	
+	let countPinsInFrame = 10
+	
 	var scoreVal = 0
 	var rollIndex = 0
+	var strikeFrameIndex : Int?
+	
 	
 	public func roll(_ pinsCount:Int) {
 		if (rollIndex < 20)
 		{
 			scoreVal += pinsCount
+			if strikeFrameIndex != nil {
+				scoreVal += pinsCount
+			}
+			if pinsCount == countPinsInFrame
+			{
+				strikeFrameIndex = rollIndex / 2
+			}
+			
 			rollIndex += 1;
 		}
 		
