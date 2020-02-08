69bb14a257f5ef34f9a4e210bdc646b31297286c
Andrew Romanov <scalli-k-be@ya.ru>
2020-02-08 17:01:29 +0700

    Initial Commit

---
+++ BowlingGameKata/BowlingGameKata.h
@@ -0,0 +1,19 @@
+//
+//  BowlingGameKata.h
+//  BowlingGameKata
+//
+//  Created by Andrew Romanov on 08.02.2020.
+//  Copyright © 2020 Andrew Romanov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BowlingGameKata.
+FOUNDATION_EXPORT double BowlingGameKataVersionNumber;
+
+//! Project version string for BowlingGameKata.
+FOUNDATION_EXPORT const unsigned char BowlingGameKataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BowlingGameKata/PublicHeader.h>
+
+
