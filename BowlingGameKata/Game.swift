7282efa1836feb9ba9a92e00d1f88de34d32bac1
Andrew Romanov <scalli-k-be@ya.ru>
2020-02-09 22:58:28 +0700

    added test for roll after end  of game after spare

--- BowlingGameKata/Game.swift
+++ BowlingGameKata/Game.swift
@@ -22,6 +22,11 @@ public class Game {
 	public func roll(_ pinsCount:Int) {
 		if (frame.frameIndex < Game.maxCountFrames || extraRollsCount > 0)
 		{
+			if (extraRollsCount > 0)
+			{
+				extraRollsCount -= 1
+			}
+			
 			let countActiveStrikes = strikes.countActiveDuplications(onRoll: rollIndex)
 			scoreVal += (pinsCount + countActiveStrikes * pinsCount)
 			
@@ -41,7 +46,14 @@ public class Game {
 				frame = Frame(frameIndex: frame.frameIndex + 1)
 				if (shouldExtraRolls)
 				{
-					extraRollsCount = 2
+					if (strikes.countActiveDuplications(onRoll: rollIndex + 2) != 0)
+					{
+						extraRollsCount = 2
+					}
+					else
+					{
+						extraRollsCount = 1
+					}
 				}
 			}
 			
