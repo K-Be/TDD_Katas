faea649eba2ef5ab670e96ce0b9da2ba1c29900c
Andrew Romanov <scalli-k-be@ya.ru>
2020-02-17 20:05:58 +0700

    Initial Commit

---
+++ StringCalculator/StringCalculator.h
@@ -0,0 +1,19 @@
+//
+//  StringCalculator.h
+//  StringCalculator
+//
+//  Created by Andrew Romanov on 17.02.2020.
+//  Copyright © 2020 Andrew Romanov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StringCalculator.
+FOUNDATION_EXPORT double StringCalculatorVersionNumber;
+
+//! Project version string for StringCalculator.
+FOUNDATION_EXPORT const unsigned char StringCalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringCalculator/PublicHeader.h>
+
+
